0c86d3d8a61d4669ee4e645f7d61dd94735accdc
swetharajagopalan17 <swetharajagopalan17@gmail.com>
2022-03-29 01:21:48 -0400

    Create packet.sv

---
+++ Gen_Drv Comm/packet.sv
@@ -0,0 +1,21 @@
+class packet;
+  
+  rand bit [7:0] inp;
+  bit [7:0] prev_inp;
+  
+  constraint valid{
+    inp inside {[0:10]};
+  }
+  constraint valid2{
+       inp != prev_inp; 
+  }
+  
+  function void post_randomize();
+    prev_inp = inp;
+  endfunction
+  
+  function void copy(packet A);
+    inp = A.inp;
+  endfunction
+  
+endclass
