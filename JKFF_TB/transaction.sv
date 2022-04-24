6dea98c5691ed04b1f7b7c102be6e1a1e852fc2a
swetharajagopalan17 <swetharajagopalan17@gmail.com>
2022-04-24 13:29:09 -0400

    Create transaction.sv

---
+++ JKFF_TB/transaction.sv
@@ -0,0 +1,12 @@
+class transaction;
+  rand bit j;
+  rand bit k;
+  //clk and reset are top level so no need to declare here 
+  bit q;
+  
+  //define constraints here 
+  
+  //printing stuff 
+  
+  
+endclass
